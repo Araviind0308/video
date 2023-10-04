c5f647cdf807e9204fedac1507eb1e9bb8013c39
Aravind M <aravindbe2016cse@gmail.com>
2023-10-04 12:51:43 +0530

    adding a another div

---
+++ src/home.js
@@ -0,0 +1,11 @@
+function home() {
+
+    const blagsc=[{title:"item1 title"},{title:"item2 title"},{title:"item3 title"}]
+    return (
+     <div>
+      <h1> Name is aravind</h1>
+     </div>
+    );
+  }
+  
+  export default home;
